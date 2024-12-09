a63e36ff7d6eb5380803fd0cb6667a343e2465a0
catwifbanana <zuwikchlopczyk@gmail.com>
2024-12-09 19:32:07 +0000

    Update memes.js

--- api/memes.js
+++ api/memes.js
@@ -3,10 +3,13 @@ import path from 'path';
 
 export default async function handler(req, res) {
     const memesDir = path.join(process.cwd(), 'public/memes');
+    const excludedMemes = ['1.jpg', '2.jpg', '3.jpg'];
 
     try {
         const files = await fs.promises.readdir(memesDir);
-        const validFiles = files.filter(file => file.match(/\.(jpg|jpeg|png|gif)$/i));
+        const validFiles = files.filter(file => 
+            file.match(/\.(jpg|jpeg|png|gif)$/i) && !excludedMemes.includes(file)
+        );
         res.status(200).json(validFiles);
     } catch (error) {
         console.error('Error reading memes directory:', error);
