c01beda24be7fcf9ddc63c2c44887beba17c132f
catwifbanana <zuwikchlopczyk@gmail.com>
2024-12-09 17:30:15 +0000

    Update data.js

--- api/data.js
+++ api/data.js
@@ -6,7 +6,7 @@ module.exports = async (req, res) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Access-Control-Allow-Methods', 'GET');
 
-        const apiUrl = 'https://app.geckoterminal.com/api/p1/solana/pools/2jRNkMgrNLEtNDfRrhhD6LicSkKkUgCd2MtfFTqDcNdZ?include=dex,dex.network.explorers,dex_link_services,network_link_services,pairs,token_link_services,tokens.token_security_metric,tokens.tags,pool_locked_liquidities&base_token=0&time_frame=24h';
+        const apiUrl = 'https://app.geckoterminal.com/api/p1/solana/pools/2jRNkMgrNLEtNDfRrhhD6LicSkKkUgCd2MtfFTqDcNdZ?include=dex,dex.network.explorers,dex_link_services,network_link_services,pairs,token_link_services,tokens.token_security_metric,tokens.tags,pool_locked_liquidities&base_token=0&time_frame=1h';
 
         const response = await axios.get(apiUrl, {
             headers: {
